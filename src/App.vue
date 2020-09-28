c0aed608d1b108fafa4e7ec70debf286ddd97425
Patrick Campbell <campbellpatrickdwight@gmail.com>
2020-09-28 14:15:10 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'Patrick',
+        firstname: 'Patrick',
+        lastname: 'Campbell',
+        email: 'patrick@patrick.com',
+        isAdmin: true
+      }
+    }
   }
 }
 </script>
